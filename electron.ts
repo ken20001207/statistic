6453dc738638d05c819d193371b3fb6214d409a4
Yuanlin Lin <im.yuanlinlin@gmail.com>
2020-04-10 20:54:24 +0800

    build macos success

--- electron.ts
+++ electron.ts
@@ -1,45 +1,47 @@
-import * as electron from "electron";
-
+import { session } from "electron";
+const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
+const path = require("path");
+const url = require("url");
+const isDev = require("electron-is-dev");
+
+let mainWindow;
+
 app.allowRendererProcessReuse = true;
 
-export var mainWindow: electron.BrowserWindow;
-
-import { session } from "electron";
-
-async function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: true,
-      webSecurity: false
-    }
-  });
-  mainWindow.loadFile("index.html");
-  mainWindow.setMenu(null);
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            webSecurity: false,
+        },
+    });
+    mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`);
+    mainWindow.on("closed", () => (mainWindow = null));
 }
 
-app.whenReady().then(() => {
-  createWindow();
+app.on("ready", () => {
+    createWindow();
 
-  session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
-    details.requestHeaders["User-Agent"] =
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36";
-    callback({ cancel: false, requestHeaders: details.requestHeaders });
-  });
+    session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
+        details.requestHeaders["User-Agent"] =
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36";
+        callback({ cancel: false, requestHeaders: details.requestHeaders });
+    });
 });
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
 app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
